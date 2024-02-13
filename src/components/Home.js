caca4448d46aa1919faffbd0a3385278b7f3c795
ChaseTeague <teaguechase@yahoo.com>
2024-02-13 00:34:02 -0500

    end of day 3 work

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home(){
             <p className="home-p">--------------- <em>All You Can Find Recipes</em> ---------------</p>
             <h5 className="home-h5">Search for a beloved recipe to cook or add your very own recipe to the collection for others to enjoy!</h5>
             <p className="p-left"><span>Giving people in the kitchen the confidence to be able to cook anything that they may desire</span></p>
-            <p className="p-right">Be sure to search through everything and enjoy your visit here at Recipe Finder</p>
+            <p className="p-right"><span>Be sure to search through everything and enjoy your visit here at Recipe Finder</span></p>
         </div>
     )
 }
