c51889e77f49b74304be1fa33ce3a146fcef061f
ChaseTeague <teaguechase@yahoo.com>
2024-02-13 00:08:23 -0500

    added more styling and edit the card shapes to not alter when clicked

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function Home(){
     return(
-        <div>
-            <h1>Recipe Finder</h1>
-            <p>Welcome to the one stop shop for all recipes!</p>
+        <div className="home">
+            <h1 className="home-title">Recipe Finder</h1>
+            <p className="home-p">--------------- <em>All You Can Find Recipes</em> ---------------</p>
+            <h5 className="home-h5">Search for a beloved recipe to cook or add your very own recipe to the collection for others to enjoy!</h5>
+            <p className="p-left"><span>Giving people in the kitchen the confidence to be able to cook anything that they may desire</span></p>
+            <p className="p-right">Be sure to search through everything and enjoy your visit here at Recipe Finder</p>
         </div>
     )
 }
